010559b3e3b6f89286d177d5df09b2068d50c7ab
KeviinPiires <k.pires81@hotmail.com>
2024-03-25 10:03:27 -0300

    fix : configurando ambiente

---
+++ src/App/Components/Header/header.jsx
@@ -0,0 +1,17 @@
+import styled from "styles-components";
+const HeaderContainer = styled.header `
+background-color: #0FA958;
+display: flex;
+justify-content: center;
+`
+function Header() {
+    return (
+        <>
+            <HeaderContainer>
+
+            </HeaderContainer>
+        </>
+    )
+}
+
+export default Header;
