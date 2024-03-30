8d3bdf687a73c0fa6eeda44b2dc0273f8d65f69c
Iago <iagomacedo159@gmail.com>
2024-03-30 16:12:57 -0300

    fix: fixing the accordion closing and adding text to the home screen

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,28 +2,27 @@ import React from 'react'
 import './NavBar.css'
 import Logo  from '../../assets/Images/Header-Img/Logo.png'
 import glass from '../../assets/Images/SVG/magnifyng_glass.svg'
-import { NavLink } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
 const NavBar = ({search}) => {
   return (
     <>
       <nav>
         <ul>
           <div className='d-flex align-items-center w-25'>
-            <img className='logoNav' src={Logo} alt="" style={{width: 64, height: 64}} />
+          <Link to={"/"}><img className='logoNav' src={Logo} alt="" style={{width: 64, height: 64}} /></Link>
             <div className="input-group">
             {search &&  
             <>
-              <span className="input-group-text" id="basic-addon1"><img src={glass} alt="" /></span>
-              <input type="text" className="form-control" placeholder="Buscar" aria-label="Buscar" aria-describedby="basic-addon1" />
+              <span className="d-none d-md-block input-group-text" id="basic-addon1"><img src={glass} alt="" /></span>
+              <input type="text" className="d-none d-md-block form-control" placeholder="Buscar" aria-label="Buscar" aria-describedby="basic-addon1" />
             </> 
             }
             </div>
           </div>
           <div className='links'>
-            <li><a href="/">Home</a></li>
-            <li><a href="/faq">FAQ</a></li>
-            <li><a href="/rank">Rank</a></li>
-            <li><a href="/login">Login</a></li>
+            <NavLink className="navLinks" exact to="/">Home</NavLink>
+            <NavLink className="navLinks" to="/faq">FAQ</NavLink>
+            <NavLink className="navLinks disabled" to="/rank">Rank</NavLink>
           </div>
         </ul>
       </nav>
