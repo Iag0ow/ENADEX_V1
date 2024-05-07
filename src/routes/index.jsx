f78cc06cdc2ca5a55d60fc2fa82ed44a4395a34d
Marcos Porto <mosther_marcos007@hotmail.com>
2024-05-07 18:44:06 -0300

    feat(components): Adiciona tela CompletedSimulations com dados fictícios

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -12,6 +12,7 @@ import WelcomeAfterLogin from "../pages/WelcomeAfterLogin/WelcomeAfterLogin";
 import { Register } from "../pages/Register";
 import { RegisterAdminTeacher } from "../pages/RegisterAdminTeacher";
 import SimulatedPanel from "./../pages/SimulatedPanel/simulatedPanel";
+import { CompletedSimulations } from "../pages/CompletedSimulations";
 function AppRoutes() {
   const { signed } = useAuth();
   return (
@@ -22,6 +23,7 @@ function AppRoutes() {
           <Route path="/register" element={signed ? <WelcomeAfterLogin /> : <Register />} />
           <Route path="/home" element={signed ? <WelcomeAfterLogin /> : <Login />} />
           <Route path="/simulados" element={signed ? <SimulatedPanel /> : <Login />} />
+          <Route path="/simulados-concluidos" element={signed ? <CompletedSimulations /> : <Login />} />
           <Route path="/esqueci-minha-senha" element={signed ? <WelcomeAfterLogin /> : <RecuperacaoSenha />} />
           {/* <Route path="/register-admin-teacher" element={signed ? <RegisterAdminTeacher /> : <Login />} /> */}
           <Route path="/register-admin-teacher" element={<RegisterAdminTeacher />} />
