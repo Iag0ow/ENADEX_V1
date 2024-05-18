78fda944c2a8141d3214d3e71319d81733e28721
Marcos Porto <mosther_marcos007@hotmail.com>
2024-05-18 18:23:21 -0300

    feat(api):consumo da api de managars e criação de managers

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -20,6 +20,7 @@ const API = "https://enadex-api-v2.vercel.app";
 //   return {...auth, status: response.status};
 // };
 
+
 export const studentRegister = async (registerForm) => {
   const bodyForm = JSON.stringify(registerForm);
   const config = {
@@ -73,4 +74,38 @@ export async function getProfile(){
   const data = await response.json();
   
   return {...data, status: response.status};
+}
+
+
+export async function registerAdminTeacher(formRegisterAdminTeacher) {
+  const bodyForm = JSON.stringify(formRegisterAdminTeacher)
+  const token = localStorage.getItem("token");
+  const config = {
+   method: "POST",
+   headers: {
+     "content-type": "application/json",
+     "Authorization": `Bearer ${token}`,
+   },
+   body: bodyForm,
+ };
+
+ const response = await fetch(`${API}/managers`, config);
+ return response;
+}
+
+export async function getManagers(){
+   const token = localStorage.getItem("token");
+   const config = {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    }
+   }
+
+   const response = await fetch(`${API}/managers`, config);
+   const data = await response.json()
+   console.log(data)
+
+   return await data
 }
