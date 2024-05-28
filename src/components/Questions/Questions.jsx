d0f4b77e446c09ba541a26346bdaa320eea1f83e
Iago <iagomacedo159@gmail.com>
2024-05-28 11:42:53 -0300

    feat: Bank question

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import QuestionsLetters from "../../assets/mock/QuestionsLetters.json";
 
-export default function Questions({ title, options, handleChange }) {
+export default function Questions({ title, options, handleChange, disabled, selectedOptionId }) {
   return (
     <div className="question-container">
-      <h5
-        className="question-title color-text"
-        dangerouslySetInnerHTML={{ __html: title }}
-      />
+      <h5 className="question-title color-text"
+      dangerouslySetInnerHTML={{ __html: title }}
+      ></h5>
       {options &&
         options.map((option, index) => (
           <div key={index} className="option d-flex align-items-center justify-content-start flex-wrap">
             <input
               type="radio"
-              value={option.correctOption}
               name={title}
               id={option._id}
               className="checkbox"
               onClick={handleChange}
+              disabled={disabled}
+              value={QuestionsLetters[index].letter}
+              {...(selectedOptionId === null ? {} : { checked: selectedOptionId === option._id })}
             />
-             &nbsp;
+            &nbsp;
             &nbsp;
             {QuestionsLetters[index].letter})
             &nbsp;
@@ -29,3 +30,4 @@ export default function Questions({ title, options, handleChange }) {
     </div>
   );
 }
+
